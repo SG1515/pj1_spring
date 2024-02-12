6d05cc36280ecd1de37bcd4e31f3819d4ea57c50
SG1515 <sunggu507@naver.com>
2024-02-12 18:50:22 +0900

    Comment : MemberServiceImpl

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,11 @@
 package hello.core.member;
+
+//메모리 버전 구현체
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
     public void join(Member member) {
+        // 호출하면 MemoryMemberRepository()에 있는 override된 save를 호출
         memberRepository.save(member);
     }
     public Member findMember(Long memberId) {
