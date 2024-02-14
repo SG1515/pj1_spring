296a4045e0eafc903414602d0703e0c2aeb3f45b
SG1515 <sunggu507@naver.com>
2024-02-14 21:22:36 +0900

    Test : 빈 상속관계 조회

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -30,6 +30,7 @@ public class ApplicationContextSameBeanFindTest {
                 MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
+
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
     void findAllBeanByType() {
