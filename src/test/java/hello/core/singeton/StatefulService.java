d16851bf16fa2f7b17c0c736661bdf14922afa4d
SG1515 <sunggu507@naver.com>
2024-02-14 23:05:53 +0900

    Refactor : stateless 상태가 되게 리팩터링

--- src/test/java/hello/core/singeton/StatefulService.java
+++ src/test/java/hello/core/singeton/StatefulService.java
@@ -2,15 +2,15 @@ package hello.core.singeton;
 
 public class StatefulService {
 
-    private int price; //상태를 유지하는 필드
+//    private int price; //상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name =" + name + " price = " + price);
-        this.price = price; //여기가 문제다
-    }
-
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 }
